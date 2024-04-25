6e59a847ccfb09484029c0d552a0783f6b9b1c26
Vinnie <80784525+TechieZombie@users.noreply.github.com>
2024-04-25 22:41:09 +0530

    Resolved the location of asset

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
-// import ServiceCard from "./ServiceCard";
-// import ReviewCard from "./ReviewCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
-export { Button, Nav, ShoeCard, PopularProductCard };
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,4 +1,4 @@
-import ServiceCard from "../components/ServiceCard";
+import ServiceCard from "../components/index";
 import { services } from "../constants";
 const Services = () => {
   return (
